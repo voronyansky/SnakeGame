f7892fda814b3f9b61ff5d12028584d6b7f4d2b1
voronyansky <voronyansky.egor@yandex.ru>
2016-06-23 12:55:01 +0400

    Small changes is desktop launcher

---
+++ desktop/src/ru/evrnsky/snake/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.evrnsky.snake.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.evrnsky.snake.SnakeGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SnakeGame(), config);
+	}
+}
